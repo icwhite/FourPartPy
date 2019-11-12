628752235d580681fccd9b69de99db50017db905
icwhite <isadoracw@berkeley.edu>
2019-11-12 11:42:40 -0800

    New Classes added

--- inputter.py
+++ inputter.py
@@ -1,1 +1,21 @@
 # inputter
+class Piece:
+    def __init__(self, num_measures, num_beats):
+        self.num_measures = num_measures
+        self.num_beats = num_beats
+        self.measures = []
+        for i in range(num_measures):
+            self.measures.append(Measure(num_beats))
+
+
+class Measure:
+    def __init__(self, num_beats):
+        self.chords = []
+        for i in range(num_beats):
+            self.chords.append(Chord())
+    def get_chord(self, ):
+
+
+class Chord:
+    '''A class representing a Chord'''
+    pass

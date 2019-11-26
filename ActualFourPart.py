9029cf9958618bde6786ffb14b89ac9cfe9f8cb4
mliu0515 <chonghualiu@berkeley.edu>
2019-11-25 22:01:41 -0800

    Update ActualFourPart.py

--- ActualFourPart.py
+++ ActualFourPart.py
@@ -1,1 +1,15 @@
 from inputter import *
+
+"""figure out ways to represent notes"""
+
+def check_paralle_fifth(Piece):
+    """do things"""
+    while :
+        if "there's 5":
+            print()
+
+def check_paralle_octave(Piece):
+    pass
+
+def has_third(Piece):
+    pass

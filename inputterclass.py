d49fa41359b64f20afd4ecc2bbf75a414438930b
icwhite <isadoracw@berkeley.edu>
2020-01-08 11:00:24 -0600

    outlining :)

--- inputterclass.py
+++ inputterclass.py
@@ -12,16 +12,46 @@ class Inputter:
         self.tempo = int(input('How many beats per minute? (i.e. 60, 120)'))
         self.piece = Piece(self.measures, self.beats_per, tempo = self.tempo)
         print("Great! Now let's start composing!")
-        self.composing(0)
+        self.composing()
 
     def create_measure(self):
         self.piece.add_measure()
 
-    def composing(self, measure):
-        '''Handles composing each measure, starting with the measure in
-        the attribute measure. If measure equals the total number of measures that
-        are in the piece, prompt to add another measure or end the piece.'''
+    def composing(self):
+        '''The method where users can decide to do one of the following things;
+            * create a new measure 'n'
+            * change the tempo of the piece 't'
+            * print out a copy of their piece so far 'p'
+            * delete a measure they created 'd' -> further prompts needed
+            * edit a measure they created 'e' '''
+        action = input('What would you like to do? Press the appropriate key:', \
+                        '\n n: create a new measure', \
+                        '\n t: change the tempo of the piece', \
+                        '\n p: print out a copy of your piece so far', \
+                        '\n d: delete a measure you have created', \
+                        '\n e: edit a measure you have created')
+        if not isinstance(action, str):
+            self.composing()
+
+    def compose_measure(self):
+        '''Creates a measure according to the user's specifications and prompts
+        for inputs. Users can do one of the following things;
+            * edit a voice of their choosing 's', 'a', 't', 'b'
+            * delete a voice of their choosing 'd' -> further prompts'''
         pass
+
+    def compose_voice(self, voice):
+        '''Editing a voice of the measure. The voice attribute is the index in
+        the measure.voices list. Start by printing the voice. Users can:
+            * add a note to the end of the measure 'a'
+            * delete a note from the end of the measure 'd' '''
+
+    def edit_measure(self, measure):
+        '''Editing a measure of the piece. The measure attribute is the index
+        of the measure in piece.measures. Start by printing the measure.
+        Users can:
+            * edit a voice 's', 'a', 't', 'b'
+            * delete a voice 'd' '''
 
     def end_of_piece(self):
         '''Print out the piece and then ask if the user would like to title it
